26f642415fcc202d57e425bfdea38395b38f5d38
Khoa Trinh <59905045+trinhcaokhoa@users.noreply.github.com>
2022-02-08 02:06:53 -0800

    Revert "Master (#7)"
    This reverts commit 6cdd9b100c0df143705086699f9209e50cb35a7a.

--- mebook_project/urls.py
+++ mebook_project/urls.py
@@ -15,8 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.storage import staticfiles_storage
-from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,10 +23,7 @@ urlpatterns = [
 
     path('', include('homepages.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/', include('users.urls')),
-    path('favicon.ico', RedirectView.as_view(
-        url=staticfiles_storage.url('images/favicon.ico')))
+    path('accounts/', include('users.urls'))
 
 ]
-
 
