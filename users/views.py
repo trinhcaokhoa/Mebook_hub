70488397178401b18fb3e2abddd2ccdad0f7e2ac
Khoa Trinh <caokhoatrinh@gmail.com>
2022-03-12 16:25:16 -0800

    FIX fix _base.html

--- users/views.py
+++ users/views.py
@@ -12,27 +12,5 @@ class SignUpPageView(generic.CreateView):
     template_name = 'signup.html'
 
 
-<<<<<<< HEAD
-
-=======
-class BookListView(ListView):
-           
-    model = MyBook
-    context_object_name = 'books_list'
-    template_name = 'mybooks/books_list.html'  
-
-    def get_queryset(self):
-        # using super() as multi-inheritance
-        qs = super().get_queryset()
-        return qs.filter(owner=self.request.user)
-    
-        
-    
-
-class BookDetailView(DetailView):
-    model = MyBook
-    context_object_name = 'book_detail' 
-    template_name = 'mybooks/book_detail.html'
->>>>>>> 1d80071910d84b5706f2b19791e2b1136c7f6753
 
 
