3b77f5940bc413ec5f54104d79e08583fdb9c486
Khoa Trinh <caokhoatrinh@gmail.com>
2022-04-01 21:48:35 -0700

    ADD download function. FIX upload function

--- library/forms.py
+++ library/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from django.forms import HiddenInput
 from .models import LibraryBook
 from django.contrib.auth import get_user_model
 
@@ -7,7 +6,9 @@ from django.contrib.auth import get_user_model
 class BookForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
        super(BookForm, self).__init__(*args, **kwargs)
-       self.fields['owner'].widget.attrs['disabled'] = True
+       self.fields['owner'].widget = forms.widgets.HiddenInput()
+    
+
     class Meta:
         model = LibraryBook     
         fields = ('owner','title', 'author', 'cover', 'file')
--- library/urls.py
+++ library/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import LibraryView,BookListView,BookDetailView,SearchView,upload_file
+
 urlpatterns = [
 
     path('', LibraryView.as_view(), name='library'),
     path('mybooks/', BookListView.as_view(), name='mybooks'),
-    path('<int:pk>', BookDetailView.as_view(), name='book_detail'),
+    path('<pk>', BookDetailView.as_view(), name='book_detail'),
     path('search/',SearchView.as_view(),name='search_results'),
-    path('upload/', upload_file, name='upload_file')
-]
+    path('upload/', upload_file, name='upload_file'),
+    path('download/', BookDetailView.download_file, name='download_file'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
