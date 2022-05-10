47e4831b86366754475469a79838c3bdcb38787b
Khoa Trinh <caokhoatrinh@gmail.com>
2022-05-09 20:26:59 -0700

    ADD secure_proxy_ssl_header UPDATE  allowed_hosts

--- mebook_project/settings.py
+++ mebook_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mebook-hub.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -190,6 +190,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True  
     SESSION_COOKIE_SECURE = True  
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Heroku
 import dj_database_url
