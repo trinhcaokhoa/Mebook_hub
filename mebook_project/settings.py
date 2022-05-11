9ff55283e592d28091c304f7c02ee104653e5869
Khoa Trinh <caokhoatrinh@gmail.com>
2022-05-11 12:46:35 -0700

    MODIFIED backend storages

--- mebook_project/settings.py
+++ mebook_project/settings.py
@@ -163,7 +163,7 @@ AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATIC_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
-DEFAULT_FILE_STORAGE = 'mysite.storage_backends.MediaStorage'
+DEFAULT_FILE_STORAGE = 'mebook_project.storage_backends.MediaStorage'
 
 # Auth setting
 AUTH_USER_MODEL = 'users.CustomUser'
