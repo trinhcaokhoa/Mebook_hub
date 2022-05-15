4afbde6fe2c43fa4808550bee8313c267414ef5f
Khoa Trinh <caokhoatrinh@gmail.com>
2022-05-14 21:57:49 -0700

    FIX staticfiles

--- mebook_project/settings.py
+++ mebook_project/settings.py
@@ -171,7 +171,6 @@ else:
     MEDIA_URL = '/media/'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 
 # Auth setting
 AUTH_USER_MODEL = 'users.CustomUser'
