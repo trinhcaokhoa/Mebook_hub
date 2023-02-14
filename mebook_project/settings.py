1ee310a76ebe2de54ba0be165eedb2bc0887877b
Khoa Trinh <caokhoatrinh@gmail.com>
2023-02-13 20:54:29 -0800

    FIX database config updated

--- mebook_project/settings.py
+++ mebook_project/settings.py
@@ -216,7 +216,7 @@ if ENVIRONMENT == 'production':
 
 # Heroku
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
 
