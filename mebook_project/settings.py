31c9c2e3e8ed3dec9b91878714a3f01e89144004
Khoa Trinh <caokhoatrinh@gmail.com>
2023-02-13 20:38:25 -0800

    MODIFIE AWS setting

--- mebook_project/settings.py
+++ mebook_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['mebook-hub.herokuapp.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['mebook-hub.herokuapp.com','mebookhub.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -156,9 +156,9 @@ if USE_S3:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    AWS_DEFAULT_ACL = None
     # s3 static settings
     # s3 static settings
     STATIC_LOCATION = 'static'
